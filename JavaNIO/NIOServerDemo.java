dcf0aa161820fcc3286e8510c147be9879d651f1
Blief <954800091@qq.com>
2018-12-09 16:27:59 +0800

    JavaNIO 初窥

---
+++ JavaNIO/NIOServerDemo.java
@@ -0,0 +1,109 @@
+package JavaNIO;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class NIOServerDemo {
+
+    public static void main(String[] args) throws IOException{
+        new Thread(new ReactorTask()).start();
+    }
+
+    public static class ReactorTask implements Runnable{
+        private Selector selector;
+
+        public ReactorTask() {
+            try{
+                //1.打开ServerSocketChannel，用于监听客户端的连接，它是所有客户端连接的父管道
+                ServerSocketChannel acceptorSvr = ServerSocketChannel.open();
+                //2.绑定监听端口
+                acceptorSvr.socket().bind(new InetSocketAddress(InetAddress.getByName("localhost"), 9999));
+                //3.设置连接为非阻塞模式
+                acceptorSvr.configureBlocking(false);
+                //4.创建Reactor线程，创建多路复用器并启动线程
+                selector = Selector.open();
+                //5.将ServerSocketChannel注册到Reactor线程的多路复用器Selector上，监听Accept事件
+                SelectionKey key = acceptorSvr.register(selector, SelectionKey.OP_ACCEPT);
+            } catch (UnknownHostException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }//ReactorTask
+
+        @Override
+        public void run() {
+            //6.在run方法中的无限循环体内，轮询准备就绪的Key
+            while (true){
+                try{
+                    selector.select(1000);
+                    Set selectedKeys = selector.selectedKeys();
+                    Iterator it = selectedKeys.iterator();
+                    SelectionKey key = null;
+                    while (it.hasNext()){
+                        key = (SelectionKey) it.next();
+                        it.remove();
+                        try{
+                            if (key.isValid()){
+                                //处理新接入的请求消息
+                                if(key.isAcceptable()){
+                                    //7.多路复用器监听到有新的客户端接入，处理新的接入请求，完成TCP三次握手，建立物理链路
+                                    ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+                                    SocketChannel sc = ssc.accept();
+                                    //8.设置客户端链路为非阻塞模式
+                                    sc.configureBlocking(false);
+                                    sc.socket().setReuseAddress(true);
+                                    //9.将新接入的客户端连接注册到Reactor线程的多路复用器上，监听读操作，读取客户端发送的网络消息
+                                    sc.register(selector, SelectionKey.OP_READ);
+                                }
+                                if(key.isReadable()){
+                                    //10.异步读取客户端请求消息到缓存区
+                                    SocketChannel sc = (SocketChannel) key.channel();
+                                    ByteBuffer readBuffer = ByteBuffer.allocate(64*1024);
+                                    int readBytes = sc.read(readBuffer);
+                                    //11.对ByteBuffer进行编解码，如果有半包消息指针reset，继续读取后续的报文
+                                    if(readBytes > 0){
+                                        readBuffer.flip();
+                                        byte[] bytes = new byte[readBuffer.remaining()];
+                                        readBuffer.get(bytes);
+                                        String body = new String(bytes, "UTF-8");
+                                        System.out.println("Server receive a order: \"" + body + "\"");
+                                        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(System.currentTimeMillis()).toString() : "OTHER ORDER";
+                                        //写应答
+                                        byte[] bytes2 = ("CurrentTime is: " + currentTime).getBytes();
+                                        ByteBuffer writeBuffer = ByteBuffer.allocate(bytes2.length);
+                                        writeBuffer.put(bytes2);
+                                        writeBuffer.flip();
+                                        sc.write(writeBuffer);
+                                    } else if(readBytes < 0) {
+                                        //对端链路关闭
+                                        key.cancel();
+                                        sc.close();
+                                    }else ;// 读到0字节，忽略
+                                }
+                            }
+                        }catch (Exception e){
+                            if(key != null){
+                                key.cancel();
+                                if(key.channel() != null)
+                                    key.channel().close();
+                            }
+                        }
+                    }//while
+                }catch (Throwable t){
+                    t.printStackTrace();
+                }
+            }//while
+        }//run
+    }
+
+}
---
+++ JavaNIO/TimeClientHandle.java
@@ -0,0 +1,130 @@
+package JavaNIO;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class TimeClientHandle implements Runnable{
+    private String host;
+    private int port;
+    private Selector selector;
+    private SocketChannel socketChannel;
+    private volatile boolean stop;
+    public TimeClientHandle(String host, int port){
+        this.host = host == null ? "127.0.0.1" : host;
+        this.port = port;
+        try{
+            //1.打开SocketChannel,用于创建客户端连接
+            socketChannel = SocketChannel.open();
+            //2.设置SocketChannel为非阻塞模式
+            socketChannel.configureBlocking(false);
+            //3.创建多路复用器（在Reactor线程中）
+            selector = Selector.open();
+        }catch (IOException e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }//TimeClientHandle
+
+    @Override
+    public void run() {
+        try{
+            //4.socketChannel发起连接
+            if(socketChannel.connect(new InetSocketAddress(host, port))){
+                //5.如果直接连接成功，则注册到多路复用器上
+                socketChannel.register(selector, SelectionKey.OP_READ);
+                //6.发送请求消息，读应答
+                byte[] req = "QUERY TIME ORDER".getBytes();
+                ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
+                writeBuffer.put(req);
+                writeBuffer.flip();
+                socketChannel.write(writeBuffer);
+                if(!writeBuffer.hasRemaining())
+                    System.out.println("Send order 2 server succeed.");
+            }else
+                socketChannel.register(selector, SelectionKey.OP_CONNECT);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        while (!stop){
+            try{
+                //7.多路复用器在run的无限循环体内轮询准备就绪的Key
+                selector.select(1000);
+                Set selectedKeys = selector.selectedKeys();
+                Iterator it = selectedKeys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()){
+                    key = (SelectionKey) it.next();
+                    it.remove();
+                    try{
+                        if(key.isValid()){
+                            //8.将连接成功的Channel注册到多路复用器上
+                            //判断是否连接成功
+                            SocketChannel sc = (SocketChannel) key.channel();
+                            if(key.isConnectable()){
+                                if(sc.finishConnect()){
+                                    sc.register(selector, SelectionKey.OP_READ);
+                                    //发送请求消息，读应答
+                                    byte[] req = "QUERY TIME ORDER".getBytes();
+                                    ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
+                                    writeBuffer.put(req);
+                                    writeBuffer.flip();
+                                    sc.write(writeBuffer);
+                                    if(!writeBuffer.hasRemaining())
+                                        System.out.println("Send order to server succeed.");
+                                }else System.exit(1);//连接失败，进程退出
+                            }
+                            //监听读操作，读取服务器端写回的网络消息
+                            if(key.isReadable()){
+                                //9.读取信息到缓冲区
+                                ByteBuffer readBuffer = ByteBuffer.allocate(64*1024);
+                                int readBytes = sc.read(readBuffer);
+                                if(readBytes > 0){
+                                    readBuffer.flip();
+                                    byte[] bytes =new byte[readBuffer.remaining()];
+                                    readBuffer.get(bytes);
+                                    String body =new String(bytes, "UTF-8");
+                                    System.out.println("Client reveive a response: \"" + body + "\"");
+                                    //this.stop =true;
+                                    //写应答
+                                    byte[] bytes2 = "QUERY TIME ORDER".getBytes();
+                                    ByteBuffer writeBuffer = ByteBuffer.allocate(bytes2.length);
+                                    writeBuffer.put(bytes2);
+                                    writeBuffer.flip();
+                                    sc.write(writeBuffer);
+                                }else if(readBytes < 0){
+                                    //对端链路关闭
+                                    key.cancel();
+                                    sc.close();
+                                }else ;//读到0字节，忽略
+                            }
+                        }
+                    }catch (Exception e){
+                        if(key != null){
+                            key.cancel();
+                            if(key.channel() != null)
+                                key.channel().close();
+                        }
+                    }
+                }//while(it.hasNext())
+            }catch (Exception e){
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }//while(!stop)
+        //多路复用器关闭后，所有注册在上面的Channel和Pipe等资源都会被自动去注册并关闭，所以不需要重复释放资源
+        if(selector != null){
+            try{
+                selector.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }//run
+}
